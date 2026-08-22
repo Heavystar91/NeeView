cd5247efbc42e080279f583fd1c399eaa64f6ae4
Heavystar91 <heavystar91@gmail.com>
2026-08-22 14:26:21 -0400

    feat: bypass NScroll for Webtoon wheel down

--- NeeView/Command/Commands/NextScrollPageCommand.cs
+++ NeeView/Command/Commands/NextScrollPageCommand.cs
@@ -22,35 +22,13 @@ namespace NeeView
 
         public override void Execute(object? sender, CommandContext e)
         {
-            var parameter = e.Parameter.Cast<ScrollPageCommandParameter>();
-
             if (BookSettings.Current.PageMode == PageMode.Webtoon)
             {
-                // OpenComic-style Webtoon navigation: move by roughly 70% of the
-                // viewport per wheel command, but animate the movement so repeated
-                // wheel input feels like one continuous strip instead of tiny steps.
-                var pagesAsOne = parameter.PagesAsOne;
-                var scrollType = parameter.ScrollType;
-                var scroll = parameter.Scroll;
-                var lineBreakStopTime = parameter.LineBreakStopTime;
-                try
-                {
-                    parameter.PagesAsOne = true;
-                    parameter.ScrollType = NScrollType.Vertical;
-                    parameter.Scroll = 0.70;
-                    parameter.LineBreakStopTime = 0.0;
-                    BookOperation.Current.Control.ScrollToNextFrame(sender, parameter);
-                }
-                finally
-                {
-                    parameter.PagesAsOne = pagesAsOne;
-                    parameter.ScrollType = scrollType;
-                    parameter.Scroll = scroll;
-                    parameter.LineBreakStopTime = lineBreakStopTime;
-                }
+                BookOperation.Current.Control.ScrollWebtoonNext(sender);
                 return;
             }
 
+            var parameter = e.Parameter.Cast<ScrollPageCommandParameter>();
             BookOperation.Current.Control.ScrollToNextFrame(sender, parameter);
         }
     }
