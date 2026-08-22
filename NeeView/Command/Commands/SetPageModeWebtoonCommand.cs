ffbdbe8819752f53f38ef46d210a52ffc90ab42d
Heavystar91 <heavystar91@gmail.com>
2026-08-22 09:30:49 -0400

    feat: add dedicated webtoon page mode command

---
+++ NeeView/Command/Commands/SetPageModeWebtoonCommand.cs
@@ -0,0 +1,30 @@
+﻿using NeeView.Properties;
+using System.Windows.Data;
+
+namespace NeeView
+{
+    public class SetPageModeWebtoonCommand : CommandElement
+    {
+        public SetPageModeWebtoonCommand()
+        {
+            this.Group = TextResources.GetString("CommandGroup.PageSetting");
+            this.ShortCutKey = new ShortcutKey("Ctrl+3");
+            this.IsShowMessage = true;
+        }
+
+        public override BindingBase CreateIsCheckedBinding()
+        {
+            return BindingGenerator.PageMode(PageMode.Webtoon);
+        }
+
+        public override bool CanExecute(object? sender, CommandContext e)
+        {
+            return BookSettings.Current.CanEdit;
+        }
+
+        public override void Execute(object? sender, CommandContext e)
+        {
+            BookSettings.Current.SetPageMode(PageMode.Webtoon);
+        }
+    }
+}
