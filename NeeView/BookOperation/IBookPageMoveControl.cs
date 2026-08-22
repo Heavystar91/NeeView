5ba05ad4f1e47778a629fe617cffe070e65d7ebf
Heavystar91 <heavystar91@gmail.com>
2026-08-22 14:40:38 -0400

    fix: keep Webtoon scrolling behind existing move interface

--- NeeView/BookOperation/IBookPageMoveControl.cs
+++ NeeView/BookOperation/IBookPageMoveControl.cs
@@ -13,8 +13,6 @@
 
         void ScrollToPrevFrame(object? sender, ScrollPageCommandParameter parameter);
         void ScrollToNextFrame(object? sender, ScrollPageCommandParameter parameter);
-        void ScrollWebtoonPrev(object? sender);
-        void ScrollWebtoonNext(object? sender);
 
         void MoveTo(object? sender, int index);
         void MoveToRandom(object? sender);
